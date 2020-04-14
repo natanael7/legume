26aebaf5b889169e82c8f8c3bba3b7e2c275b438
Natanael Marian <m.natanail7@gmail.com>
2020-04-14 22:41:47 +0300

    fully responsive, minor bugs

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,19 +1,33 @@
-// $(".button").click(function () {
-//   $("#myForm").fadeIn();
-//   $("html body").addClass("locked");
-//   $("#overlay").fadeIn();
-// });
-// $(".button--close").click(function () {
-//   $("#myForm").fadeOut();
-//   $("html body").removeClass("locked");
-//   $("#overlay").fadeOut();
-// });
 $(".button").click(function () {
-  $("#salad").fadeIn()
+  $("#myForm").fadeIn();
+  $("html body").addClass("locked");
   $("#overlay").fadeIn();
 });
-// $(".recipe-close").click(function () {
-//   $("#myForm").fadeOut();
-//   $("html body").removeClass("locked");
-//   $("#overlay").fadeOut();
-// });
+$(".button--close").click(function () {
+  $("#myForm").fadeOut();
+  $("html body").removeClass("locked");
+  $("#overlay").fadeOut();
+});
+
+$("#salad_button").click(function () {
+  $("#salad").fadeIn()
+  $("html body").addClass("locked");
+  $("#overlay").fadeIn();
+});
+$("#salad_close").click(function () {
+  $("#salad").fadeOut();
+  $("html body").removeClass("locked");
+  $("#overlay").fadeOut();
+});
+
+$("#potatoes_button").click(function () {
+  $("#potatoes").fadeIn();
+  $("html body").addClass("locked");
+  $("#overlay").fadeIn();
+});
+$("#potatoes_close").click(function () {
+  $("#potatoes").fadeOut();
+  $("html body").removeClass("locked");
+  $("#overlay").fadeOut();
+});
+
